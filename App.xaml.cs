cca0a9da21c586b1a8a354c93ab769bc4a3d5d8a
Ankita <A00305054@mycambrian.ca>
2024-11-17 20:29:54 -0500

    Modify Mauiprogram.cs file

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using localbusinessExplore.Pages;
+
+namespace localbusinessExplore;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+
+        MainPage = new SignUpPage();
+    }
+}
